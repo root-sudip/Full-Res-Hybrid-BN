50a58571353dcc03a07f71935804cb048f6bb896
Sudip Das <d.sudip47@gmail.com>
2019-03-25 21:49:44 +0530

    updated

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #<network filename> <training filename> <test filename> <load weight(Load/New)> <task(Train/Predict)>
-python Hybrid_Model_DOCR_full-res-bn.py Data/TestGAN_950_icdar2019.h5 Data/TestGAN_950_icdar2019.h5 Load Predict
+python Hybrid_Model_DOCR_full-res-bn.py Data/Train.h5 Data/TestGAN_950_icdar2019.h5 Load Predict #For test
 python evaluate_CER_from_prediction.py
 
